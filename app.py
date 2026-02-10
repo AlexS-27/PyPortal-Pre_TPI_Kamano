c56790785bf75cb100e4223500bf4f1c28b13437
AlexS-27 <akamanopro@gmail.com>
2026-02-10 08:24:34 +0100

    Refactor: add more security about the login and register

--- app.py
+++ app.py
@@ -1,35 +1,56 @@
 import os
-
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from werkzeug.security import check_password_hash
 from core.db_manager import get_user_by_username, register_user
+from functools import wraps
+from core.utils import is_password_strong
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+app.config['PERMANENT_SESSION_LIFETIME'] = 1800 #the session expire after 30 minutes
+
+# With the AI help
+# Manage the fact that the user need to be connect to access to the page
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('You must be logged in to access this page', 'danger')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
+@login_required
 def home():
  return "Hello World! PyPortal is running!"
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form['username'].strip()
+        password = request.form['password'].strip()
 
         user = get_user_by_username(username)
+        is_strong, message = is_password_strong(password)
+
+        if not is_strong:
+            flash(message, 'danger')
+            return render_template('register.html')
 
         if register_user(username, password):
             flash('You have successfully registered! Now you can login!', 'success')
             return redirect(url_for('login'))
         else:
-            flash('The username already exist. Please select an other one.', 'danger')
+            flash("There's been an error, please retry", 'danger')
             return render_template('register.html')
+
+    return render_template("register.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form['username'].strip()
+        password = request.form['password'].strip()
 
         user = get_user_by_username(username)
 
